8eec23bb77cc67f24a4739c7a49150a493b83c3d
christian <habinezac15@gmail.com>
2022-02-15 22:39:14 +0200

    new features added

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(cors());
 // Add middware for parsing request bodies here:
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
