e2f2b265b540758b4b07be6c3096719fed3e268c
christian <habinezac15@gmail.com>
2022-02-17 20:57:54 +0200

    new changes added

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
