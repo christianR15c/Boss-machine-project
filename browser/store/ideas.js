bc17b07df4f5326aa0e7112841ed9c4cb8f84199
christian <habinezac15@gmail.com>
2022-02-18 10:05:34 +0200

    new changes added

--- browser/store/ideas.js
+++ browser/store/ideas.js
@@ -8,65 +8,70 @@ const UPDATE_IDEA = 'UPDATE_IDEA';
 
 // actions
 
-export const setIdeas = ideas => {
+export const setIdeas = (ideas) => {
   return {
     type: SET_IDEAS,
-    ideas
-  }
-}
+    ideas,
+  };
+};
 
-export const createIdea = idea => {
+export const createIdea = (idea) => {
   return {
     type: CREATE_IDEA,
-    idea
-  }
-}
+    idea,
+  };
+};
 
-export const updateIdea = idea => {
+export const updateIdea = (idea) => {
   return {
     type: UPDATE_IDEA,
     idea,
-  }
-}
+  };
+};
 
 // Thunks
 
-export const createIdeaThunk = idea => dispatch => {
-  axios.post('http://localhost:4001/api/ideas', idea)
-  .then(res => res.data)
-  .then(createdIdea => {
-    dispatch(createIdea(createdIdea));
-  })
-  .catch(console.error.bind(console));
-}
+export const createIdeaThunk = (idea) => (dispatch) => {
+  axios
+    .post('http://boss-mchine.herokuapp.com/api/ideas', idea)
+    .then((res) => res.data)
+    .then((createdIdea) => {
+      dispatch(createIdea(createdIdea));
+    })
+    .catch(console.error.bind(console));
+};
 
-export const updateIdeaThunk = idea => dispatch => {
-  axios.put(`http://localhost:4001/api/ideas/${idea.id}`, idea)
-  .then(res => res.data)
-  .then(updatedIdea => {
-    dispatch(updateIdea(updatedIdea));
-    dispatch(setSelectedIdea(updatedIdea));
-  });
-}
-
+export const updateIdeaThunk = (idea) => (dispatch) => {
+  axios
+    .put(`http://boss-mchine.herokuapp.com/api/ideas/${idea.id}`, idea)
+    .then((res) => res.data)
+    .then((updatedIdea) => {
+      dispatch(updateIdea(updatedIdea));
+      dispatch(setSelectedIdea(updatedIdea));
+    });
+};
 
 // Reducer
 
 const initial = [];
 
 export default (initialState = initial, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case CREATE_IDEA:
       return [...initialState, action.idea];
     case SET_IDEAS:
       return action.ideas;
     case UPDATE_IDEA:
-      const index = initialState.findIndex(el => el.id === action.idea.id);
+      const index = initialState.findIndex((el) => el.id === action.idea.id);
       if (index === -1) {
         return initialState;
       }
-      return [...initialState.slice(0, index), action.idea, ...initialState.slice(index + 1)];
+      return [
+        ...initialState.slice(0, index),
+        action.idea,
+        ...initialState.slice(index + 1),
+      ];
     default:
       return initialState;
   }
-}
+};
